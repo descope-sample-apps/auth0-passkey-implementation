0b3d2e69f03a55b03c10a6cd0c29b24a2e72bf7a
Kevin Gao <kevinjamesgao@gmail.com>
2023-06-08 16:23:12 -0700

    added oidc-flow.json

--- src/views/DescopeLogin.js
+++ src/views/DescopeLogin.js
@@ -3,7 +3,7 @@ import { Descope } from "@descope/react-sdk";
 export const DescopeLogin = () => {
   return (
     <Descope
-      flowId="sign-up-or-in"
+      flowId="oidc-flow"
       onSuccess={(e) => console.log(e.detail.user)}
       onError={(e) => console.log("Could not log in!")}
       theme="light"
